d9b434d2e3c2d0ca6e7d5e35e9256f3bed79a415
joelsander11 <70577171+joelsander11@users.noreply.github.com>
2025-03-12 14:34:18 +0100

    Updated query to now correctly rank similar images.
    Updated query to now correctly identify similar images with the highest resolution and number of unique colors. The images are ranked by resolution and unique color count, and filter groups with more than one image per hash value.

--- data/queries/similarImagesRanked.sql
+++ data/queries/similarImagesRanked.sql
@@ -1,26 +1,35 @@
-SELECT
-    e.filePath,
-    e.XResolution,
-    e.YResolution
-FROM
-    exifData e
-JOIN
-    similarImages s ON e.filePath = s.filePath
-WHERE
-    (e.XResolution, e.YResolution) IN (
-        SELECT
-            MAX(e2.XResolution),
-            MAX(e2.YResolution)
-        FROM
-            exifData e2
-        JOIN
-            similarImages s2 ON e2.filePath = s2.filePath
-        WHERE
-            s2.hashValue = s.hashValue
-        GROUP BY
-            s2.hashValue
+WITH RankedFiles AS (
+    SELECT 
+        s.filePath, 
+        s.hashType,
+        s.hashValue, 
+        e.XResolution, 
+        e.YResolution, 
+        u.numUniqueColors,
+        DENSE_RANK() OVER (
+            PARTITION BY s.hashValue 
+            ORDER BY 
+                CASE 
+                    WHEN e.XResolution IS NOT NULL AND e.YResolution IS NOT NULL 
+                    THEN e.XResolution * e.YResolution
+                    ELSE 0
+                END DESC,
+
+                CASE 
+                    WHEN u.numUniqueColors IS NOT NULL THEN u.numUniqueColors
+                    ELSE 0
+                END DESC
+        ) AS rank
+    FROM similarImages s
+    JOIN exifdata e ON s.filePath = e.filePath
+    LEFT JOIN uniqueColorData u ON s.filePath = u.filePath
+    WHERE s.hashValue IN (
+        SELECT hashValue
+        FROM similarImages
+        GROUP BY hashValue
+        HAVING COUNT(hashValue) > 1
+        )
     )
-ORDER BY
-    s.hashValue,    
-    e.XResolution DESC,
-    e.YResolution DESC;
+    SELECT * 
+    FROM RankedFiles
+    ORDER BY hashValue ASC, rank ASC;
